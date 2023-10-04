3b45e57791fc8ca0972bf8676b8d5dbb25a1d97d
Marcelo Lima de Souza <tr20703ps@trf1.gov.br>
2023-10-04 09:51:01 -0300

    criando containers

---
+++ product-api/src/test/java/br/com/demoproductapi/DemoProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demoproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
